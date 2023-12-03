dec7d2b4758fadd3bac3a307249eb3c3f78920e1
ucode0101 <ucode0101@gmail.com>
2023-12-02 20:49:50 -0500

    added more

--- src/test/java/com/ucodeacademy/utilities/Driver.java
+++ src/test/java/com/ucodeacademy/utilities/Driver.java
@@ -19,138 +19,59 @@ public class Driver {
 
     }
 
-//    private static WebDriver driver;
-//
-//    public static WebDriver getDriver(){
-//
-//        if (driver == null){
-//
-//
-//            String browserName = ConfigReader.getProperty("browser");
-//            browserName = browserName.toLowerCase();
-//
-//            switch (browserName){
-//
-//                case "chrome":
-//                    driver = new ChromeDriver();
-//                    break;
-//                case "firefox":
-//                    driver = new FirefoxDriver();
-//                    break;
-//                case "safari":
-//                    driver = new SafariDriver();
-//                    break;
-//                case "chrome-headless":
-//
-//                   ChromeOptions options = new ChromeOptions();
-//                   options.addArguments("--headless");
-//
-//                   driver = new ChromeDriver(options);
-//                   break;
-//                case "chrome-remote":
-//
-//                    DesiredCapabilities capabilities = new DesiredCapabilities();
-//                    capabilities.setBrowserName("chrome");
-//                    capabilities.setPlatform(Platform.ANY);
-//                    try {
-//
-//                        URL url = new URL("http://localhost:4444/wd/hub");
-//                        threadLocalDriver.set(new RemoteWebDriver(url,capabilities));
-//
-//                    } catch (Exception e){
-//                        e.getStackTrace();
-//                        throw new RuntimeException("Remote Web Driver is not working");
-//                    }
-//                    break;
-//                case "edge":
-//                    driver = new EdgeDriver();
-//                    break;
-//                default:
-//                    System.out.println("Invalid browser name");
-//
-//
-//            }
-//            driver.manage().window().maximize();
-//            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//
-//        }
-//        return driver;
-//
-//    }
-//
-//
-//    public static void quitDriver(){
-//        if (driver !=null){
-//            driver.quit();
-//            driver = null;
-//        }
-//    }
+    private static WebDriver driver;
+
+    public static WebDriver getDriver(){
+
+        if (driver == null){
 
 
-    private static ThreadLocal<WebDriver> threadLocalDriver = new ThreadLocal<>();
+            String browserName = ConfigReader.getProperty("browser");
+            browserName = browserName.toLowerCase();
 
-    public static WebDriver getDriver() {
-        // Check if the threadLocalDriver has already been initialized for this thread
-        if (threadLocalDriver.get() == null) {
-            // If not initialized, create a new instance based on the browser name
-            String browserName = ConfigReader.getProperty("browser").toLowerCase();
+            switch (browserName){
 
-            switch (browserName) {
                 case "chrome":
-                    threadLocalDriver.set(new ChromeDriver());
+                    driver = new ChromeDriver();
                     break;
                 case "firefox":
-                    threadLocalDriver.set(new FirefoxDriver());
+                    driver = new FirefoxDriver();
                     break;
                 case "safari":
-                    threadLocalDriver.set(new SafariDriver());
-                    break;
-                case "edge":
-                    threadLocalDriver.set(new EdgeDriver());
+                    driver = new SafariDriver();
                     break;
                 case "chrome-headless":
-                    ChromeOptions options = new ChromeOptions();
-                    options.addArguments("--headless");
-                    threadLocalDriver.set(new ChromeDriver(options));
+
+                   ChromeOptions options = new ChromeOptions();
+                   options.addArguments("--headless");
+
+                   driver = new ChromeDriver(options);
+                   break;
+                case "edge":
+                    driver = new EdgeDriver();
                     break;
-                case "chrome-remote":
-
-                    DesiredCapabilities capabilities = new DesiredCapabilities();
-                    capabilities.setBrowserName("chrome");
-                    capabilities.setPlatform(Platform.ANY);
-                    try {
-
-                        URL url = new URL("http://localhost:4444/wd/hub");
-                        threadLocalDriver.set(new RemoteWebDriver(url,capabilities));
-
-                    } catch (Exception e){
-                        e.getStackTrace();
-                        throw new RuntimeException("Remote Web Driver is not working");
-                    }
-                    break;
-
                 default:
-                    threadLocalDriver.set(new ChromeDriver());
+                    System.out.println("Invalid browser name");
+
+
             }
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         }
-
-        // Configure the WebDriver instance
-        WebDriver driver = threadLocalDriver.get();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
         return driver;
+
     }
 
-    // Add a method to quit the WebDriver instance at the end of the test
-    public static void quitDriver() {
-        WebDriver driver = threadLocalDriver.get();
-        if (driver != null) {
+
+    public static void quitDriver(){
+        if (driver !=null){
             driver.quit();
-            // Remove the WebDriver instance from the ThreadLocal to avoid memory leaks
-            threadLocalDriver.remove();
+            driver = null;
         }
     }
+
+
 
 
 }
